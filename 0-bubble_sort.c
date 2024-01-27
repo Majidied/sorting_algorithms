200fbe5466769012ab5ba928d64358118251ca88
mohammed majidi <majidimajidi2003@outlook.com>
2024-01-27 23:19:23 +0100

    task0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,21 +1,25 @@
 #include "sort.h"
 
-void bubble_sort(int *array, size_t size) {
-  int tmp;
-  for (size_t i = 0; i < size - 1; ++i) {
-    bool swapped = false;
-    for (size_t j = 0; j < size - i - 1; ++j) {
-      if (array[j] > array[j + 1]) {
-        tmp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = tmp;
-        swapped = true;
-        print_array(array, size);
-      }
+void bubble_sort(int *array, size_t size)
+{
+    int tmp;
+    for (size_t i = 0; i < size - 1; ++i)
+    {
+        bool swapped = false;
+        for (size_t j = 0; j < size - i - 1; ++j)
+        {
+            if (array[j] > array[j + 1])
+            {
+                tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+                swapped = true;
+                print_array(array, size);
+            }
+        }
+        /* If no two elements were swapped by inner loop, then the array is sorted.
+         */
+        if (!swapped)
+            break;
     }
-    /* If no two elements were swapped by inner loop, then the array is sorted.
-     */
-    if (!swapped)
-      break;
-  }
 }

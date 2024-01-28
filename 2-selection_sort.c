d4e550ff958599a37dce2df0cd8606b3147a344f
mohammed majidi <majidimajidi2003@outlook.com>
2024-01-28 16:47:02 +0100

    task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -24,6 +24,9 @@ void Swap(int *arr, int i, int j)
 void selection_sort(int *array, size_t size)
 {
     size_t i, j, min;
+    
+    if (size < 2 || array == NULL)
+    return;
 
     for (i = 0; i < size - 1; i++)
     {
